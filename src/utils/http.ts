87cd344cab534e64cf6466e2639ea860861f0763
ardhendu_init_ <ardhendujj002@gmail.com>
2023-09-13 15:38:06 +0530

    (bug-fix) Base url has been removed from env

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,8 @@
 import Axios from "axios";
 
+const BASE_URL = "http://localhost:3000/api";
 export const serverAxios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
