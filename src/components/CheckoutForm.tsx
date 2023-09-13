1783884c6492d1d0c4495018119c959da1203213
ardhendu_init_ <ardhendujj002@gmail.com>
2023-09-13 15:56:59 +0530

    minor prisma issue fixes

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -62,7 +62,8 @@ const CheckoutForm = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/success",
+        // return_url: "http://localhost:3000/success",
+        return_url: "/",
       },
     });
 
